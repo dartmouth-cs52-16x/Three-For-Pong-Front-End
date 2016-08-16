20e45fea3908f33fd932c5457ba9ee341eab1fb2
Devina.Kumar.18@dartmouth.edu <devinakumar@devinas-air.wireless.dartmouth.edu>
2016-08-15 23:11:14 -0400

    changed files

--- GoneNative/index.ios.js
+++ GoneNative/index.ios.js
@@ -36,12 +36,13 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: 'black',
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
+    color: 'red',
   },
   instructions: {
     textAlign: 'center',
